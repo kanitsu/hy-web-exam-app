a2fca8a30bd30ec9729ae209b11bbf955e6a85c2
Charlie Irawan <kanitsu@gmail.com>
2023-03-31 09:19:21 +0800

    Add info when no more video

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import React, { useState } from 'react';
 import { Home } from './Home';
 import { TabBar } from './components/TabBar';
 import { Discover } from './Discover';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function App() {
   const [active, setActive] = useState('Home');
   return (
-    <View style={styles.container} >
-      {active == 'Discover' && <Discover />}
-      {active == 'Home' && <Home />}
-      <TabBar labels={['Home', 'Discover']} onChange={setActive} />
-    </View>
+    <RootSiblingParent>
+      <View style={styles.container} >
+        {active == 'Discover' && <Discover />}
+        {active == 'Home' && <Home />}
+        <TabBar labels={['Home', 'Discover']} onChange={setActive} />
+      </View>
+    </RootSiblingParent>
   );
 }
 
@@ -20,7 +23,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#fff',
+    backgroundColor: '#777',
     alignItems: 'center',
     justifyContent: 'center',
   },
